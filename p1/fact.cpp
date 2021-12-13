ea228f6d726d88b0998cb4cbc7f544ebf09cc014
Jake Crouch <flamefire333@gmail.com>
2021-12-13 07:05:34 +0000

    Initial setup for p1

---
+++ p1/fact.cpp
@@ -0,0 +1,12 @@
+/* 
+ * fact is a function that should return the factorial of n
+ * Some example values:
+ * 0! = 1
+ * 1! = 1
+ * 2! = 2 * 1! = 2
+ * 3! = 3 * 2! = 6
+ * 4! = 4 * 3! = 24
+ */
+int fact(int n) {
+	// TODO Code for calculating n! and returning it
+}
---
+++ p1/t1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int fact(int);
+int main() {
+	std::cout << fact(0) << "\n";
+	std::cout << fact(1) << "\n";
+	std::cout << fact(2) << "\n";
+	std::cout << fact(3) << "\n";
+	std::cout << fact(4) << "\n";
+	std::cout << fact(5) << "\n";
+	std::cout << fact(6) << "\n";
+	std::cout << fact(7) << "\n";
+}
